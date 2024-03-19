53a80cc51ab0b3a020de1371b09e21da02e8954a
choilhyun <cihg155@gmail.com>
2024-03-19 18:00:47 +0900

    [코딩 인터뷰] 예제

---
+++ src/CodingInterview/StringMain.java
@@ -0,0 +1,10 @@
+package CodingInterview;
+
+public class StringMain {
+    public static void main(String[] args) {
+        String[] aa = new String[0];
+        aa[1] = "fsdf";
+
+        System.out.println(aa[1]);
+    }
+}
---
+++ src/CodingInterview/sample.java
@@ -0,0 +1,16 @@
+package CodingInterview;
+
+public class sample {
+    private static final String strTest = "fsdfsfsdf";
+    public static void main(String[] args) {
+        String result = test1();
+
+        System.out.println(result);
+    }
+
+    public static String test1(){
+        String result = "";
+
+        return result;
+    }
+}
